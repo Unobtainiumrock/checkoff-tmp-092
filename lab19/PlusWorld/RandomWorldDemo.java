47485470f36164d6684a33d3b2d54c32eaddb008
Nicholas Fleischahuer <unobtainiumrock@gmail.com>
2022-07-26 00:24:36 -0700

    Surprise mothafucka

--- lab19/PlusWorld/RandomWorldDemo.java
+++ lab19/PlusWorld/RandomWorldDemo.java
@@ -18,6 +18,7 @@ public class RandomWorldDemo {
 
     /**
      * Fills the given 2D array of tiles with RANDOM tiles.
+     *
      * @param tiles
      */
     public static void fillWithRandomTiles(TETile[][] tiles) {
@@ -30,28 +31,49 @@ public class RandomWorldDemo {
         }
     }
 
-    /** Picks a RANDOM tile with a 33% change of being
-     *  a wall, 33% chance of being a flower, and 33%
-     *  chance of being empty space.
+    public static void fillFlower(TETile[][] tiles, int n) {
+        for (int i = 0; i < tiles.length; i++) {
+            for (int j = 0; j < tiles[0].length; j++) {
+                if ((n <= i && i < 2 * n && j < 3 * n) || (n <= j && j < 2 * n && i < 3 * n)) {
+                    tiles[i][j] = Tileset.FLOWER;
+                } else {
+                    tiles[i][j] = Tileset.NOTHING;
+                }
+            }
+        }
+    }
+
+    /**
+     * Picks a RANDOM tile with a 33% change of being
+     * a wall, 33% chance of being a flower, and 33%
+     * chance of being empty space.
      */
     private static TETile randomTile() {
         int tileNum = RANDOM.nextInt(3);
         switch (tileNum) {
-            case 0: return Tileset.WALL;
-            case 1: return Tileset.FLOWER;
-            case 2: return Tileset.NOTHING;
-            default: return Tileset.NOTHING;
+            case 0:
+                return Tileset.WALL;
+            case 1:
+                return Tileset.FLOWER;
+            case 2:
+                return Tileset.NOTHING;
+            default:
+                return Tileset.NOTHING;
         }
     }
 
+
     public static void main(String[] args) {
         TERenderer ter = new TERenderer();
-        ter.initialize(WIDTH, HEIGHT);
+        ter.initialize(15, 15);
 
-        TETile[][] randomTiles = new TETile[WIDTH][HEIGHT];
-        fillWithRandomTiles(randomTiles);
+//        TETile[][] randomTiles = new TETile[WIDTH][HEIGHT];
+//        fillWithRandomTiles(randomTiles);
+        TETile[][] flower = new TETile[15][15];
+        fillFlower(flower, 3);
 
-        ter.renderFrame(randomTiles);
+        ter.renderFrame(flower);
+//        ter.renderFrame(randomTiles);
     }
 
 
